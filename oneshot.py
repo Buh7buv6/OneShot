7a3467be4488831f3493afe8a7bd68af1d86effd
Buh7buv6 <88053126+Buh7buv6@users.noreply.github.com>
2024-10-03 21:09:15 +0530

    Update oneshot.py

--- oneshot.py
+++ oneshot.py
@@ -997,7 +997,7 @@ class WiFiScanner:
         for n, network in network_list_items:
             number = f'{n})'
             model = '{} {}'.format(network['Model'], network['Model number'])
-            essid = truncateStr(network['ESSID','Unknown'], 25)
+            essid = truncateStr(network.get('ESSID','Unknown'), 25)
             deviceName = truncateStr(network['Device name'], 27)
             line = '{:<4} {:<18} {:<25} {:<8} {:<4} {:<27} {:<}'.format(
                 number, network['BSSID'], essid,
@@ -1010,6 +1010,7 @@ class WiFiScanner:
             except Exception as e:
                 if (network['BSSID']) in self.stored:
                     print('ESSID UNKNOWN'+colored(line, color='yellow'))
+                    pass
                 elif network['WPS locked']:
                     print(colored(line, color='red'))
                 elif self.vuln_list and (model in self.vuln_list):
